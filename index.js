6786e9b130ac34082ea8cc5cadf3443bf0b6a256
Александр Канов <a.kanov@akanov.local>
2018-08-09 14:48:53 +0300

    commit

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 
 app.use(express.static('public'))
-app.get(/test[1-9]$/, function(req, res){
+app.get('/:landing_name', function(req, res) {
   res.sendFile('/public/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on *:3000');
 });
